316bc8c1d46b381ef1f8087e67c0bd4889ac16b0
Gustav Onsberg <gustavonsberg@gmail.com>
2023-01-20 17:37:28 +0100

    JSON object is now dynamic

--- DataFetch/DataFetcher.cs
+++ DataFetch/DataFetcher.cs
@@ -1,3 +1,6 @@
+
+using Newtonsoft.Json.Linq;
+
 class DataFetcher{
 	public static async Task<StockInfo> stock(String ticker, String exchange){
 		StockInfo result = new StockInfo();
@@ -5,19 +8,32 @@ class DataFetcher{
 
 		HttpResponseMessage quoteSummaryRes = await client.GetAsync("https://query1.finance.yahoo.com/v11/finance/quoteSummary/" + ticker + "?modules=assetProfile");
 		String quoteSummaryJson = await quoteSummaryRes.Content.ReadAsStringAsync();
-		QuoteSummaryRoot quoteSummaryRoot =  System.Text.Json.JsonSerializer.Deserialize<QuoteSummaryRoot>(quoteSummaryJson);
+		dynamic quoteSummary = JObject.Parse(quoteSummaryJson);
+
 
 		HttpResponseMessage quoteRes = await client.GetAsync("https://query1.finance.yahoo.com/v6/finance/quote?symbols=" + ticker);
 		String quoteJson = await quoteRes.Content.ReadAsStringAsync();
-		QuoteRoot quoteRoot =  System.Text.Json.JsonSerializer.Deserialize<QuoteRoot>(quoteJson);
+		dynamic quote = JObject.Parse(quoteJson);
+
+		String exchangeTranslated = quote.quoteResponse.result[0].exchange;
+
+		System.Console.WriteLine(quote.quoteResponse.result[0].exchange);
+		System.Console.WriteLine("test22");
+
+		String yfExchange = quote.quoteResponse.result[0].exchange;
+
+		switch(""+quote.quoteResponse.result[0].exchange){
+			case "NMS": exchangeTranslated = "NASDAQ"; break;
+			case "NYQ": exchangeTranslated = "NYSE"; break;
+		}
 
 		result.ticker = ticker;
-		result.exchange = exchange;
-		result.name = quoteRoot.quoteResponse.result[0].shortName;
-        result.industry = quoteSummaryRoot.quoteSummary.result[0].assetProfile.industry;
-        result.sector = quoteSummaryRoot.quoteSummary.result[0].assetProfile.sector;
-        result.website = quoteSummaryRoot.quoteSummary.result[0].assetProfile.website;
-        result.country = quoteSummaryRoot.quoteSummary.result[0].assetProfile.country;
+		result.exchange = exchangeTranslated;
+		result.name = quote.quoteResponse.result[0].shortName;
+        result.industry = quoteSummary.quoteSummary.result[0].assetProfile.industry;
+        result.sector = quoteSummary.quoteSummary.result[0].assetProfile.sector;
+        result.website = quoteSummary.quoteSummary.result[0].assetProfile.website;
+        result.country = quoteSummary.quoteSummary.result[0].assetProfile.country;
 
 		return result;
 	}
