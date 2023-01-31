325ee581c5b6b7f185ab5266d3302e495e1d430e
Gustav Onsberg <gustavonsberg@gmail.com>
2023-01-31 19:04:39 +0100

    Made todo in search.cs

--- BackendService/Endpoints/Search/Search.cs
+++ BackendService/Endpoints/Search/Search.cs
@@ -3,9 +3,12 @@ class Search
 {
 	public static async Task<SearchResponse> endpoint(SearchBody body) //TODO: not done at all
 	{
-		SearchResponse searchResponse = new SearchResponse("error");
+		SearchResponse searchResponse = new SearchResponse("error", "");
 
 		await YfSearch.searchStocksAsync(body.term);
+
+		// TODO: search hour own database here (after the yahoo search)
+		//This function should return a JSON string with results.
 
 		return searchResponse;
 	}
@@ -13,12 +16,14 @@ class Search
 
 class SearchResponse
 {
-	public SearchResponse(string response)
+	public SearchResponse(string response, string results)
 	{
 		this.response = response;
+		this.results = results;
 	}
 
 	public String response { get; set; }
+	public String results { get; set; }
 }
 
 class SearchBody
