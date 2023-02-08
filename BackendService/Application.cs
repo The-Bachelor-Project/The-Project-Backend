a62ec11733b327d0d0e86c40fff1da5685b2fdf1
CookieNess <frederikkjaergaard@yahoo.dk>
2023-02-08 20:06:30 +0100

    Fixed CORS problem

--- BackendService/Application.cs
+++ BackendService/Application.cs
@@ -2,6 +2,6 @@ namespace BackendService;
 
 class Application
 {
-	static WebApplicationBuilder builder = WebApplication.CreateBuilder();
-	public static WebApplication app = builder.Build();
+	public static WebApplicationBuilder builder = WebApplication.CreateBuilder();
+	public static WebApplication app;
 }
--- BackendService/Program.cs
+++ BackendService/Program.cs
@@ -8,9 +8,22 @@ class Program
 {
 	public static void Main()
 	{
-		Endpoints.setup();
 
+		var allowCORS = "_allowCors";
+		Application.builder.Services.AddCors(options =>
+		{
+			options.AddPolicy(name: allowCORS,
+							  policy =>
+							  {
+								  policy.WithOrigins("*").AllowAnyHeader().AllowAnyMethod();
+							  });
+		});
+		Application.builder.Services.AddControllers();
+		Application.app = Application.builder.Build();
+		Application.app.UseCors(allowCORS);
+		Endpoints.setup();
 		Application.app.Run();
+
 	}
 }
 
