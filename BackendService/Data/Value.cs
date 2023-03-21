3356bf851359f11bd97e66301576b432a2f98da3
Gustav Onsberg <gustavonsberg@gmail.com>
2023-03-21 12:17:30 +0100

    Update Value.cs

--- BackendService/Data/Value.cs
+++ BackendService/Data/Value.cs
@@ -2,12 +2,12 @@ namespace Data;
 
 class Value
 {
-	public Value(decimal amount, Currency currency)
+	public Value(decimal amount, String currency)
 	{
 		Amount = amount;
 		Currency = currency;
 	}
 
 	public Decimal Amount { get; set; }
-	public Currency Currency { get; set; }
+	public String Currency { get; set; }
 }
