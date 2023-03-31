3b4b8c8ea2b173ce8eaa9ffc7f77d271007880ee
Gustav Onsberg <gustavonsberg@gmail.com>
2023-03-31 17:05:30 +0200

    Update GetStockHistories.cs

--- BackendService/APIv1/Endpoints/StockHistories/GetStockHistories.cs
+++ BackendService/APIv1/Endpoints/StockHistories/GetStockHistories.cs
@@ -4,13 +4,13 @@ class GetStockHistories
 {
 	public static void Setup(WebApplication app)
 	{
-		app.MapGet("/v1/stock-histories", ([FromQuery] string ticker, string exchange, string startDate, string endDate, string interval, string token) =>
+		app.MapGet("/v1/stock-histories", ([FromQuery] string ticker, string exchange, string startDate, string endDate, string interval, string accessToken) =>
 		{
-			return Results.Ok(Endpoint(ticker, exchange, startDate, endDate, interval, token));
+			return Results.Ok(Endpoint(ticker, exchange, startDate, endDate, interval, accessToken));
 		});
 	}
 
-	public static GetStockHistoriesResponse Endpoint(string ticker, string exchange, string startDate, string endDate, string interval, string token)
+	public static GetStockHistoriesResponse Endpoint(string ticker, string exchange, string startDate, string endDate, string interval, string accessToken)
 	{
 		return new GetStockHistoriesResponse("success", new Data.StockHistory("1","2","3","4","5"));
 	}
