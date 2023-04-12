992c0896b8a21bcce81f4d1f006c236643f8c688
CookieNess <frederikkjaergaard@yahoo.dk>
2023-04-12 17:48:20 +0200

    Fixed null error when getting currencies

--- BackendService/Data/YahooFinace/CurrencyHistory.cs
+++ BackendService/Data/YahooFinace/CurrencyHistory.cs
@@ -12,7 +12,6 @@ class CurrencyHistory : ICurrencyHistory
 		HttpClient Client = new HttpClient();
 
 		String url = "https://query1.finance.yahoo.com/v7/finance/download/" + currency + "USD=X" + "?interval=1d&period1=" + StartTime + "&period2=" + EndTime;
-
 		HttpResponseMessage StockHistoryRes = await Client.GetAsync(url);
 		String StockHistoryCsv = await StockHistoryRes.Content.ReadAsStringAsync();
 
@@ -22,7 +21,15 @@ class CurrencyHistory : ICurrencyHistory
 		for (int i = 1; i < DataLines.Length; i++)
 		{
 			String[] Data = DataLines[i].Split(",");
-			Result.History = Result.History.Append(new Data.CurrencyHistoryData(DateOnly.Parse(Data[0]), Decimal.Parse(Data[1]), Decimal.Parse(Data[2]), Decimal.Parse(Data[3]), Decimal.Parse(Data[4]))).ToArray();
+			if (Data[1] == "null")
+			{
+				CurrencyHistoryData temp = new Data.CurrencyHistoryData(DateOnly.Parse(Data[0]), Result.History.Last().OpenPrice, Result.History.Last().HighPrice, Result.History.Last().LowPrice, Result.History.Last().ClosePrice);
+				Result.History = Result.History.Append(temp).ToArray();
+			}
+			else
+			{
+				Result.History = Result.History.Append(new Data.CurrencyHistoryData(DateOnly.Parse(Data[0]), Decimal.Parse(Data[1]), Decimal.Parse(Data[2]), Decimal.Parse(Data[3]), Decimal.Parse(Data[4]))).ToArray();
+			}
 		}
 		return Result;
 	}
