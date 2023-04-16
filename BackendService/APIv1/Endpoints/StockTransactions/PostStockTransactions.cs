9d02d51be99f2ac7f02911ee75697fba2d67c86d
CookieNess <frederikkjaergaard@yahoo.dk>
2023-04-16 14:27:52 +0200

    stocktransactions wip

--- BackendService/APIv1/Endpoints/StockTransactions/PostStockTransactions.cs
+++ BackendService/APIv1/Endpoints/StockTransactions/PostStockTransactions.cs
@@ -6,12 +6,29 @@ class PostStockTransactions
 	{
 		app.MapPost("/v1/stock-transactions", (PostStockTransactionsBody body) =>
 		{
-			return Results.Ok(Endpoint(body));
+			return Results.Ok(EndpointAsync(body));
 		});
 	}
 
-	public static PostStockTransactionsResponse Endpoint(PostStockTransactionsBody body)
+	public static async Task<PostStockTransactionsResponse> EndpointAsync(PostStockTransactionsBody body)
 	{
-		return new PostStockTransactionsResponse("success", body.accessToken);
+		BusinessLogic.User user = new BusinessLogic.TokenSet(body.accessToken).GetUser();
+		System.Console.WriteLine("User: " + user.Id);
+		BusinessLogic.StockTransaction StockTransaction = new BusinessLogic.StockTransaction();
+		System.Console.WriteLine("----------------------------------");
+		System.Console.WriteLine(body.stockTransaction.portfolio);
+		StockTransaction.PortfolioId = body.stockTransaction.portfolio;
+		StockTransaction.Ticker = body.stockTransaction.ticker;
+		StockTransaction.Exchange = body.stockTransaction.exchange;
+		StockTransaction.Amount = body.stockTransaction.amount;
+		StockTransaction.Timestamp = body.stockTransaction.timestamp;
+		StockTransaction.Currency = body.stockTransaction.currency;
+		StockTransaction.Price = body.stockTransaction.price;
+		await StockTransaction.AddToDb();
+		if (StockTransaction.Id != null)
+		{
+			return new PostStockTransactionsResponse("success", StockTransaction.Id);
+		}
+		return new PostStockTransactionsResponse("error", null);
 	}
 }
