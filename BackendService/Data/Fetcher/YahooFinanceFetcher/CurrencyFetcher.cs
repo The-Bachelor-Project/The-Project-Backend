4c3de5d76608b19867274fe1e415c9eb4f322046
CookieNess <frederikkjaergaard@yahoo.dk>
2023-04-26 12:13:50 +0200

    Fixed GBX conversion

--- BackendService/Data/Fetcher/YahooFinanceFetcher/CurrencyFetcher.cs
+++ BackendService/Data/Fetcher/YahooFinanceFetcher/CurrencyFetcher.cs
@@ -14,7 +14,12 @@ public class CurrencyFetcher : ICurrencyFetcher
 		int endTime = Tools.TimeConverter.dateOnlyToUnix(endDate);
 
 		HttpClient client = new HttpClient();
-
+		Boolean isGBX = false;
+		if (currency.ToUpper() == "GBX")
+		{
+			currency = "GBP";
+			isGBX = true;
+		}
 		String url = "https://query1.finance.yahoo.com/v7/finance/download/" + currency + "USD=X" + "?interval=1d&period1=" + startTime + "&period2=" + endTime;
 		System.Console.WriteLine(url);
 		HttpResponseMessage stockHistoryRes = await client.GetAsync(url);
@@ -22,20 +27,52 @@ public class CurrencyFetcher : ICurrencyFetcher
 
 		String[] dataLines = stockHistoryCsv.Replace("\r", "").Split("\n");
 
-		Data.CurrencyHistory result = new Data.CurrencyHistory(currency, startDate, endDate, "daily");
-		for (int i = 1; i < dataLines.Length; i++)
+		Data.CurrencyHistory? result = null;
+
+		if (isGBX)
 		{
-			String[] data = dataLines[i].Split(",");
-			if (data[1] == "null")
+			result = new Data.CurrencyHistory("GBX", startDate, endDate, "daily");
+			for (int i = 1; i < dataLines.Length; i++)
 			{
-				Data.DatePrice temp = new Data.DatePrice(DateOnly.Parse(data[0]), result.history.Last().openPrice, result.history.Last().highPrice, result.history.Last().lowPrice, result.history.Last().closePrice);
-				result.history.Add(temp);
-			}
-			else
-			{
-				result.history.Add(new Data.DatePrice(DateOnly.Parse(data[0]), new StockApp.Money(Decimal.Parse(data[1])), new StockApp.Money(Decimal.Parse(data[2])), new StockApp.Money(Decimal.Parse(data[3])), new StockApp.Money(Decimal.Parse(data[4]))));
+				String[] data = dataLines[i].Split(",");
+				if (data[1] == "null")
+				{
+					Data.DatePrice temp = new Data.DatePrice(DateOnly.Parse(data[0]), result.history.Last().openPrice, result.history.Last().highPrice, result.history.Last().lowPrice, result.history.Last().closePrice);
+					temp.openPrice.amount /= 100;
+					temp.highPrice.amount /= 100;
+					temp.lowPrice.amount /= 100;
+					temp.closePrice.amount /= 100;
+					result.history.Add(temp);
+				}
+				else
+				{
+					Data.DatePrice temp = new Data.DatePrice(DateOnly.Parse(data[0]), new StockApp.Money(Decimal.Parse(data[1])), new StockApp.Money(Decimal.Parse(data[2])), new StockApp.Money(Decimal.Parse(data[3])), new StockApp.Money(Decimal.Parse(data[4])));
+					temp.openPrice.amount /= 100;
+					temp.highPrice.amount /= 100;
+					temp.lowPrice.amount /= 100;
+					temp.closePrice.amount /= 100;
+					result.history.Add(temp);
+				}
 			}
 		}
+		else
+		{
+			result = new Data.CurrencyHistory(currency, startDate, endDate, "daily");
+			for (int i = 1; i < dataLines.Length; i++)
+			{
+				String[] data = dataLines[i].Split(",");
+				if (data[1] == "null")
+				{
+					Data.DatePrice temp = new Data.DatePrice(DateOnly.Parse(data[0]), result.history.Last().openPrice, result.history.Last().highPrice, result.history.Last().lowPrice, result.history.Last().closePrice);
+					result.history.Add(temp);
+				}
+				else
+				{
+					result.history.Add(new Data.DatePrice(DateOnly.Parse(data[0]), new StockApp.Money(Decimal.Parse(data[1])), new StockApp.Money(Decimal.Parse(data[2])), new StockApp.Money(Decimal.Parse(data[3])), new StockApp.Money(Decimal.Parse(data[4]))));
+				}
+			}
+		}
+
 		return result;
 	}
 }
