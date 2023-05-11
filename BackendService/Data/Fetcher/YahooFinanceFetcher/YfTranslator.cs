42429b5d9cff42b380f01e0970171aff99be08c0
CookieNess <frederikkjaergaard@yahoo.dk>
2023-05-11 22:28:51 +0200

    Added new exchanges

--- BackendService/Data/Fetcher/YahooFinanceFetcher/YfTranslator.cs
+++ BackendService/Data/Fetcher/YahooFinanceFetcher/YfTranslator.cs
@@ -11,16 +11,45 @@ public class YfTranslator
 		{"IOB","STO"},
 		{"TOR","TSE"}
 	};
-	public static Dictionary<String, String> stockSymbolExtension = new Dictionary<string, string>(){
-		{"FRA",".F"},
+	public static Dictionary<string, string> stockSymbolExtension = new Dictionary<string, string>()
+	{
+		{"FRA", ".F"},
 		{"ETR", ".DE"},
-		{"CPH",".CO"},
-		{"NYSE",""},
-		{"NASDAQ",""},
-		{"STO",".ST"},
-		{"TSE",".TO"},
+		{"CPH", ".CO"},
+		{"NYSE", ""},
+		{"NASDAQ", ""},
+		{"STO", ".ST"},
+		{"TSE", ".TO"},
 		{"HEL", ".HE"},
-		{"LON", ".L"}
+		{"LON", ".L"},
+		{"BCBA", ".BA"},
+		{"VIE", ".VI"},
+		{"ASX", ".AX"},
+		{"EBR", ".BR"},
+		{"BVMF", ".SA"},
+		{"SWX", ".SW"},
+		{"SHA", ".SS"},
+		{"SHE", ".SZ"},
+		{"HKG", ".HK"},
+		{"IDX", ".JK"},
+		{"TLV", ".TA"},
+		{"BOM", ".BO"},
+		{"NSE", ".NS"},
+		{"ICE", ".IC"},
+		{"TYO", ".T"},
+		{"KORSDAQ", ".KQ"},
+		{"KRX", ".KS"},
+		{"RSE", ".RG"},
+		{"BMV", ".MX"},
+		{"AMS", ".AS"},
+		{"NZE", ".NZ"},
+		{"WSE", ".WA"},
+		{"ELI", ".LS"},
+		{"SGX", ".SI"},
+		{"BKK", ".BK"},
+		{"IST", ".IS"},
+		{"TPE", ".TW"},
+		{"TPO", ".TWO"}
 	};
 
 	public static String GetYfSymbol(String ticker, String exchange)
